3ed3a5c99ed3d602b3eca44993f96a4c5f8612ba
Vasso <vl00033@surrey.ac.uk>
2012-01-03 18:06:44 +0000

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'rails/all'
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
@@ -8,12 +9,12 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-if defined?(Bundler)
+Bundler.require(:default, Rails.env) if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
-end
+#end
 
 module SampleApp1
   class Application < Rails::Application
