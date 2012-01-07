8e5294bbaab140785c480e910c1408a711a820f3
vl00033 <vl00033@surrey.ac.uk>
2012-01-07 20:24:05 +0000

    Done with sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
-
-  # Return a title on a per-page basis.
+# Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App1"
+    base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
       base_title
     else
@@ -10,4 +9,7 @@ module ApplicationHelper
     end
   end
 
+def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,57 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'digest'
 class User < ActiveRecord::Base
-  attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation
+ attr_accessor(:password)
 
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ attr_accessible(:name, :email, :password, :password_confirmation)
 
-  validates :name,  :presence => true,
-                    :length   => { :maximum => 50 }
-  validates :email, :presence => true,
-                    :format   => { :with => email_regex },
-		    :uniqueness => { :case_sensitive => false }
+ email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  # Automatically create the virtual attribute 'password_confirmation'.
-  validates :password, :presence     => true,
-                       :confirmation => true,
-                       :length       => { :within => 6..40 }
+ validates(:name, :presence=>true, :length=>{:maximum=>50})
+ 
+ validates(:email, :presence=>true, :format=>{:with => email_regex}, :uniqueness=>{:case_sensitive=>false})
 
-  before_save :encrypt_password
-   # Return true if the user's password matches the submitted password.
+ validates(:password, :presence =>true,
+		      :confirmation=>true,
+                      :length=>{:within=>6..40})
+
+before_save(:encrypt_password)
+
+# Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
-    encrypted_password == encrypt(submitted_password)
-  end
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
 
-  def self.authenticate(email, submitted_password)
+	encrypted_password==encrypt(submitted_password)
+  
+end
+
+def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
   end
 
-  private
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
 
-    def encrypt_password
-     self.salt = make_salt unless has_password?(password) 
-     self.encrypted_password = encrypt(password)
+private
+
+   def encrypt_password
+      self.salt = make_salt unless has_password?(self.password)
+      self.encrypted_password = encrypt(self.password) #the same with self.encrypted_password = encrypt(password)
     end
 
     def encrypt(string)
@@ -45,16 +65,6 @@ class User < ActiveRecord::Base
     def secure_hash(string)
       Digest::SHA2.hexdigest(string)
     end
-end
 
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
+end
 
