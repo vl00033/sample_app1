247d068cb9caa16abb1fe991453de5fef8084bed
vl00033 <vl00033@surrey.ac.uk>
2012-01-11 12:40:19 +0000

    deleted heroku folder

--- heroku/ruby/1.9.1/gems/therubyracer-heroku-0.8.1.pre3/ext/v8/v8_str.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RUBY_V8_STR_
-#define _RUBY_V8_STR_
-
-#include "rr.h"
-
-void rr_init_str();
-
-VALUE rr_reflect_v8_string(v8::Handle<v8::Value> value);
-#endif
--- heroku/ruby/1.9.1/gems/therubyracer-heroku-0.8.1.pre3/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-
-require 'v8'
-
-require 'erb'
-def rputs(msg)
-  puts "<pre>#{ERB::Util.h(msg)}</pre>"
-  $stdout.flush
-end
